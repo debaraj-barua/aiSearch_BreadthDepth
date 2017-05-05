d345bfa7075e8a7c977b950afdb01da1f1910241
Debaraj Barua <debaraj.barua@smail.inf.h-brs.de>
2017-05-05 11:10:02 +0200

    first push

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * Daniel Vázquez
+ * Aritifiacial Intelligence for Robots
+ * MAS SS 16
+ * Assignment 3
+ *
+ * main.cpp
+ * */
+
+#include "environment.hpp"
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+int main(int arc, char* argv[])
+
+{
+	Environment anEnvironment;
+	anEnvironment.run();
+	return 1;
+}
